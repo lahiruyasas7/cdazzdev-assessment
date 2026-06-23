792dc419ebf8237ac2318020a86f46883c2c963a
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-06-23 21:10:18 +0530

    chore: setup swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from 'config/swagger-config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  // ── Swagger (only in non-production environments)
+  if (!isProd) {
+    setupSwagger(app);
+  }
+
+  // API prefix
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
