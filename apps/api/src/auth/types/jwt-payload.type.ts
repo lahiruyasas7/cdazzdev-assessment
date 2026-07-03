f121130817ac6cdda64c09980029615a4ba4aa02
Lahiru Yasas <60518183+lahiruyasas7@users.noreply.github.com>
2026-07-03 17:23:49 +0530

    Revert "Enhance authentication, project, task, and comment modules"

--- apps/api/src/auth/types/jwt-payload.type.ts
+++
@@ -1,8 +0,0 @@
-import { GlobalRole } from "src/generated/prisma/enums";
-
-
-export interface JwtPayload {
-  sub: string; // user id
-  email: string;
-  role: GlobalRole;
-}
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { QueryProvider } from "@/lib/api/query-provider";
-import { AuthProvider } from "@/lib/auth/auth-context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <QueryProvider>
-          <AuthProvider>{children}</AuthProvider>
-        </QueryProvider>
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
